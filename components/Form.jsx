41cb7df0735a557587cd40923b63ec2f0fc25a80
emege93 <emege93@gmail.com>
2021-12-08 17:03:37 +0100

    add form component

---
+++ components/Form.jsx
@@ -0,0 +1,83 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+const Form = ({ formData, forNewMovie = true }) => {
+  const router = useRouter();
+  const [form, setform] = useState({
+    title: formData.title,
+    plot: formData.plot,
+  });
+  const [message, setMessage] = useState([]);
+
+  const handleOnChange = (e) => {
+    const { value, name } = e.target;
+    setform({ ...form, [name]: value });
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (forNewMovie) {
+      postData(form);
+    } else {
+      // editar data
+    }
+  };
+
+  const postData = async (form) => {
+    try {
+      console.log(form);
+
+      const res = await fetch("/api/movie", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+
+      const data = await res.json();
+      console.log(data);
+
+      if (!res.ok) {
+        throw new Error(res.status);
+      }
+
+      router.push("/");
+    } catch (error) {
+      setMessage("Failed to add Movie");
+    }
+  };
+
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <input
+        type="text"
+        className="form-control my-2"
+        placeholder="Title"
+        autoComplete="off"
+        name="title"
+        value={form.title}
+        onChange={handleOnChange}
+      />
+      <input
+        type="text"
+        className="form-control my-2"
+        placeholder="Plot"
+        autoComplete="off"
+        name="plot"
+        value={form.plot}
+        onChange={handleOnChange}
+      />
+      <button className="btn btn-primary w-100" type="submit">
+        Add
+      </button>
+      <Link href="/">
+        <a className="btn btn-warning w-100 my-2">Back</a>
+      </Link>
+      <p>{message}</p>
+    </form>
+  );
+};
+
+export default Form;
--- pages/new.jsx
+++ pages/new.jsx
@@ -1,80 +1,15 @@
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import Form from "../components/Form";
 
 const New = () => {
-  const router = useRouter();
-  const [form, setform] = useState({
+  const formData = {
     title: "",
     plot: "",
-  });
-  const [message, setMessage] = useState([]);
-
-  const handleOnChange = (e) => {
-    const { value, name } = e.target;
-    setform({ ...form, [name]: value });
-  };
-
-  const handleOnSubmit = (e) => {
-    e.preventDefault();
-    postData(form);
-  };
-
-  const postData = async (form) => {
-    try {
-      console.log(form);
-
-      const res = await fetch("/api/movie", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(form),
-      });
-
-      const data = await res.json();
-      console.log(data);
-
-      if (!res.ok) {
-        throw new Error(res.status);
-      }
-
-      router.push("/");
-    } catch (error) {
-      setMessage("Failed to add Movie");
-    }
   };
 
   return (
     <div className="container">
       <h1 className="my-3">Add Movie</h1>
-      <form onSubmit={handleOnSubmit}>
-        <input
-          type="text"
-          className="form-control my-2"
-          placeholder="Title"
-          autoComplete="off"
-          name="title"
-          value={form.title}
-          onChange={handleOnChange}
-        />
-        <input
-          type="text"
-          className="form-control my-2"
-          placeholder="Plot"
-          autoComplete="off"
-          name="plot"
-          value={form.plot}
-          onChange={handleOnChange}
-        />
-        <button className="btn btn-primary w-100" type="submit">
-          Add
-        </button>
-        <Link href="/">
-          <a className="btn btn-warning w-100 my-2">Back</a>
-        </Link>
-        <p>{message}</p>
-      </form>
+      <Form formData={formData} />
     </div>
   );
 };
